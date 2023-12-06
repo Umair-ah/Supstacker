bd6056ba6f3f5ec3c17402269c9ceed93ff03e1b
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-12-06 18:37:15 +0530

    relationships of models and some minor adjustments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_person_name
   has_one_attached :avatar
 
+  has_many :stacks
+  has_many :products, through: :stacks
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
